d7a81e9364b8718d3b2ccce3c32d9d5ba6be1228
Park JaeHong <blogerhong@naver.com>
2024-12-30 18:18:39 +0900

    chore : Init Commit

---
+++ src/test/java/com/jhpark/dockertest/DockertestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhpark.dockertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockertestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
